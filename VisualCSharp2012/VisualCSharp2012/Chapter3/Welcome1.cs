98204e0215fed809f135af085b46f9a7911a2031
LAPTOP-TCOKUI7B\olaez <olaez_ernesto@hotmail.com>
2021-04-20 00:05:57 -0500

    Add the first class for Welcome

---
+++ VisualCSharp2012/VisualCSharp2012/Chapter3/Welcome1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VisualCSharp2012.Chapter3
+{
+    public class Welcome1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# Programming!");
+            Console.ReadLine();
+        }
+    }
+}
